7600607405da9d5ba8011e0ef13f4d458ddaa4b9
Alessio Scaiola <a.scaiola@gmail.com>
2023-06-16 22:40:17 +0200

    dev - created ranking wrapper

---
+++ src/views/Ranking.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="ft__ranking-view">
+    <ranking-table-vue></ranking-table-vue>
+  </div>
+</template>
+<script>
+import RankingTableVue from '../components/RankingTable.vue'
+export default {
+  name: 'RankingView',
+  components: {
+    RankingTableVue
+  }
+}
+</script>
