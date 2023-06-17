81dc09cfc87f66c66ab50faadfd9f8d73814b5c7
Alessio Scaiola <a.scaiola@gmail.com>
2023-06-17 12:55:14 +0200

    dev - created dynamic columns object based on the table visible fields

--- src/components/RankingTable.vue
+++ src/components/RankingTable.vue
@@ -1,31 +1,28 @@
 <template>
-    <DataTable :value="rankingData">
-      <Column field="intRank">
-      </Column>
-      <Column field="strTeamBadge">
-        <template #body="slotProps">
-            <img :src="slotProps.data.strTeamBadge" :alt="`${slotProps.data.strTeam} crest`" />
-        </template>
-      </Column>
-      <Column field="strTeam"></Column>
-      <Column field="strTeamBadge">
-        <template #body="slotProps">
-            <form-icon
-              v-for="(icon, index) in slotProps.data.strForm.split('')"
-              :key="`${icon}-${index}`"
-              :form-type="icon">
-            </form-icon>
-        </template>
-      </Column>
-      <Column field="intPlayed" header="GP"></Column>
-      <Column field="intWin" header="W"></Column>
-      <Column field="intDraw" header="D"></Column>
-      <Column field="intLoss" header="L"></Column>
-      <Column field="intGoalsFor" header="GF"></Column>
-      <Column field="intGoalsAgainst" header="GA"></Column>
-      <Column field="intGoalDifference" header="GD"></Column>
-      <Column field="intPoints" header="Pts"></Column>
-    </DataTable>
+  <DataTable :value="rankingData">
+    <Column field="intRank">
+    </Column>
+    <Column field="strTeamBadge">
+      <template #body="slotProps">
+        <img :src="slotProps.data.strTeamBadge" :alt="`${slotProps.data.strTeam} crest`" />
+      </template>
+    </Column>
+    <Column field="strTeam"></Column>
+    <Column field="strTeamBadge">
+      <template #body="slotProps">
+        <form-icon v-for="(icon, index) in slotProps.data.strForm.split('')" :key="`${icon}-${index}`" :form-type="icon">
+        </form-icon>
+      </template>
+    </Column>
+    <Column field="intPlayed" header="GP"></Column>
+    <Column field="intWin" header="W"></Column>
+    <Column field="intDraw" header="D"></Column>
+    <Column field="intLoss" header="L"></Column>
+    <Column field="intGoalsFor" header="GF"></Column>
+    <Column field="intGoalsAgainst" header="GA"></Column>
+    <Column field="intGoalDifference" header="GD"></Column>
+    <Column field="intPoints" header="Pts"></Column>
+  </DataTable>
 </template>
 <script>
 import apis from '@/constants/apis';
@@ -42,7 +39,21 @@ export default {
   data () {
     return {
       rankingData: [],
-      isFetching: false
+      isFetching: false,
+      visibleFields: [
+        'intRank',
+        'strTeamBadge',
+        'strTeam',
+        'strForm',
+        'intPlayed',
+        'intWin',
+        'intDraw',
+        'intLoss',
+        'intGoalsFor',
+        'intGoalsAgainst',
+        'intGoalDifference',
+        'intPoints',
+      ]
     }
   },
   mounted () {
@@ -62,7 +73,41 @@ export default {
     async fetchData () {
       const response = await fetch(apis.RANKING);
       const parsedResponse = await response.json();
-      this.rankingData = [...parsedResponse.table ];
+      this.rankingData = [...parsedResponse.table];
+    },
+    mapResponsePropsToTableHeaderFields (field) {
+      switch (field) {
+        case 'intGoalsAgainst':
+          return 'GA';
+        case 'intPoints':
+          return 'Pts';
+        case 'intGoalDifference':
+          return 'GD';
+        case 'intPlayed':
+          return 'GP';
+        case 'intGoalsFor':
+          return 'GF';
+        case 'intDraw':
+          return 'D';
+        case 'intLoss':
+          return 'L';
+        case 'intWin':
+          return 'W';
+        case 'strForm':
+          return 'Form';
+        default:
+          return '';
+      }
+    }
+  },
+  computed: {
+    rankingTableColumns () {
+      return this.visibleFields.map((field) => {
+          return {
+            field,
+            header: this.mapResponsePropsToTableHeaderFields(field)
+          };
+        });
     }
   }
 }
