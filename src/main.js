b8f3971bfa3d66eddeb51eff29aac771aa287d8e
Alessio Scaiola <a.scaiola@gmail.com>
2023-06-18 17:05:15 +0200

    ref - removed unecessary store

--- src/main.js
+++ src/main.js
@@ -1,12 +1,10 @@
 import App from '@/App.vue';
 import PrimeVue from 'primevue/config';
 import Vue from 'vue';
-import store from './store';
 
 Vue.config.productionTip = false
 Vue.use(PrimeVue);
 
 new Vue({
-  store,
   render: h => h(App)
 }).$mount('#app')
