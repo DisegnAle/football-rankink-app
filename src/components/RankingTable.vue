d12fa3eca16362e40e0d2cab01eabde57ca29b9b
Alessio Scaiola <a.scaiola@gmail.com>
2023-06-18 11:56:10 +0200

    dev - styled ranking table

--- src/components/RankingTable.vue
+++ src/components/RankingTable.vue
@@ -1,12 +1,14 @@
 <template>
-  <DataTable removableSort :value="data" class="p-datatable-sm">
-    <Column :sortable="isSortingBtnShown(col.field)" v-for="col of tableColumns" :field="col.field" :header="col.header" :key="col.field">
+  <DataTable removableSort :value="data" class="p-datatable-sm fr-ranking-table">
+    <Column :sortable="isSortingBtnShown(col.field)" v-for="col of tableColumns" :field="col.field" :header="col.header"
+      :key="col.field">
       <template #body="slotProps">
         <div v-if="col.field === 'strTeamBadge'">
           <img :src="slotProps.data.strTeamBadge" :alt="`${slotProps.data.strTeam} crest`" />
         </div>
         <div v-else-if="col.field === 'strForm'">
-          <form-icon v-for="(icon, index) in slotProps.data.strForm.split('')" :key="`${icon}-${index}`" :form-type="icon">
+          <form-icon v-for="(icon, index) in slotProps.data.strForm.split('')" :key="`${icon}-${index}`"
+            :form-type="icon">
           </form-icon>
         </div>
         <span v-else-if="col.field === 'intRank'" :id="`row-${slotProps.data.intRank}`">
@@ -41,9 +43,39 @@ export default {
     }
   },
   methods: {
-    isSortingBtnShown(field){
+    isSortingBtnShown (field) {
       return !['strTeamBadge', 'strForm', 'intPlayed', 'intRank', 'strTeam'].includes(field)
     }
   }
 }
 </script>
+
+<style lang="scss">
+.fr-ranking-table {
+  &.p-datatable {
+    .p-datatable-tbody {
+      &>tr {
+        &>td {
+          .p-column-title {
+            font-weight: 600;
+          }
+        }
+      }
+    }
+  }
+}
+
+@media screen and (max-width: 960px) {
+  .fr-ranking-table {
+    &.p-datatable.p-datatable-sm {
+      .p-datatable-tbody {
+        &>tr {
+          &>td {
+            padding: 0.5rem 3rem;
+          }
+        }
+      }
+    }
+  }
+}
+</style>
